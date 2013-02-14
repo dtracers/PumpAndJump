f34d7c54d00969d6adeee3745539c70a3c6a216a
dtracers <dtracers@gmail.com>
2013-02-14 13:47:57 -0600

    saved our units info

--- PumpAndJump/src/com/musicgame/PumpAndJump/LevelInterpreter.java
+++ PumpAndJump/src/com/musicgame/PumpAndJump/LevelInterpreter.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 
 public class LevelInterpreter
 {
+	/**
+	 * Returns the list of the GameObjects that were loaded from the level
+	 * @return
+	 */
 	public ArrayList<GameObject> loadLevel()
 	{
 		return null;
---
+++ PumpAndJump/src/com/musicgame/PumpAndJump/Units.java
@@ -0,0 +1,15 @@
+package com.musicgame.PumpAndJump;
+
+/**
+ *
+ * @author gigemjt
+ *
+ */
+public class Units
+{
+	/**
+	 * Furlongs! - a unit of distance
+	 *
+	 * Tocks - a unit of time
+	 */
+}
