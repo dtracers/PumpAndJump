ff06c4c84ece237a7b5cb865d051826e3e17fba5
Christopher A. Trask <chris.trask@gmail.com>
2013-04-02 15:50:20 -0500

    Graham Scan done
    Only tested on a very simple case tho

--- PumpAndJump/src/com/musicgame/PumpAndJump/Util/IntersectionUtil.java
+++ PumpAndJump/src/com/musicgame/PumpAndJump/Util/IntersectionUtil.java
@@ -1,6 +1,7 @@
 package com.musicgame.PumpAndJump.Util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
@@ -13,6 +14,15 @@ public class IntersectionUtil {
 	 */
 	public static void main(String[] args) {
 		// TODO Testing algorithms
+		float[] fpoints = { 0.0f, 0.0f, 2.0f, 0.0f, 1.0f, 1.0f, 2.0f, 2.0f, 0.0f, 2.0f };
+		Vector2[] points = FloatToVector2( fpoints );
+		
+		Vector2[] ch= grahamScan( points );
+		
+		for( int i = 0; i < ch.length; i++ )
+		{
+			System.out.println( ch[i].x + "," + ch[ i ].y );
+		}
 	}
 	
 	/*--------------------------------------------------------------------------------
@@ -42,12 +52,11 @@ public class IntersectionUtil {
 		if( ( orig.length & 0x01 ) != 0 )
 			return null;
 		
-		Vector2[] copy = new Vector2[ orig.length>>1 ];
+		Vector2[] copy = new Vector2[ (orig.length>>1) ];
 		
 		for( int i = 0; i < copy.length; i++ )
 		{
-			copy[i].x = orig[2*i];
-			copy[i].y = orig[2*i+1];
+			copy[i] = new Vector2( orig[2*i], orig[2*i+1] );
 		}
 		
 		return copy;
@@ -68,7 +77,7 @@ public class IntersectionUtil {
 	/*-------------------------------------------------------------------------------------
 	 * 		Less Useful Helpers
 	 *-----------------------------------------------------------------------------------*/
-	
+	//Returns the Vector2 with the lowest value of y
 	private static Vector2 findVector2WithMinY( Vector2[] points )
 	{
 		Vector2 min = points[0];
@@ -78,6 +87,70 @@ public class IntersectionUtil {
 				min = points[i];
 		}
 		return min;
+	}
+	
+	//Helper class to sort the points rotationally
+	//Class assumes min is found with findVector2WithMinY and is correct
+	//Allows me to use java's sort and is faster because the angles are not recomputed
+	private static class rotationalPointValue implements Comparable< rotationalPointValue >
+	{
+		public Vector2 point;
+		public float angle;
+		
+		public rotationalPointValue( Vector2 point, Vector2 min )
+		{
+			this.point = point;
+			
+			float xDist = point.x - min.x;
+			float yDist = point.y - min.y;
+			
+			if( xDist == 0 && yDist == 0 )
+				angle = -1;
+			else
+			{
+				//Compute the hypotenuse of the triangle that was created
+				float h = (float)Math.sqrt( xDist*xDist + yDist*yDist );
+				//Find the adjacent edge to the angle which is to be found
+				float a = xDist;
+				//Compute angle using inverse cosine 
+				angle = (float)Math.acos( a/h );
+			}
+		}
+		
+		public int compareTo( rotationalPointValue point ){
+	       return Float.compare( angle, point.angle );
+	    }
+		
+		public String toString()
+		{
+			return angle+" ";
+		}
+	}
+	
+	//sorts in the Vector2 array points
+	//assumes min is found with findVector2WithMinY and is correct
+	private static void sortPointsRotationallyAroundMin( Vector2[] points, Vector2 min )
+	{
+		rotationalPointValue[] rotPoints = new rotationalPointValue[ points.length ];
+		for( int i = 0; i < rotPoints.length; i++ )
+		{
+			rotPoints[i] = new rotationalPointValue( points[i], min );
+		}
+		
+		
+		
+		Arrays.sort( rotPoints );
+		
+		for( int i = 0; i < points.length; i++ )
+		{
+			points[i] = rotPoints[i].point;
+		}
+	}
+	
+	//Checks to see if the angle between 3 points is counter clockwise
+	private static boolean isClockwise( Vector2 p1, Vector2 p2, Vector2 p3)
+	{
+	    return ( ( ( p2.x - p1.x )*( p3.y - p1.y ) - ( p2.y - p1.y )*( p3.x - p1.x ) ) <= 0 );
 	}
 	
 	/*-------------------------------------------------------------------------------------
@@ -96,12 +169,41 @@ public class IntersectionUtil {
 		Vector2 min = findVector2WithMinY( points );
 		
 		ArrayList< Vector2 > inConvexHull = new ArrayList< Vector2 >();
+
+		
+		sortPointsRotationallyAroundMin( points, min );
 		inConvexHull.add( min );
+		inConvexHull.add( points[1] );
 		
+		//actual algorithm
+		for( int i = 2; i < points.length; i++ )
+		{
+			while ( isClockwise( inConvexHull.get( inConvexHull.size() - 2 ), inConvexHull.get( inConvexHull.size() - 1 ), points[i] ) )
+			{
+				if( inConvexHull.size() > 1 )
+				{
+					inConvexHull.remove( inConvexHull.size() - 1 );
+				}
+			}   
+			
+			inConvexHull.add( points[i] );  
+		}
 		
+		/*while ( isClockwise( inConvexHull.get( inConvexHull.size() - 3 ), inConvexHull.get( inConvexHull.size() - 2 ), inConvexHull.get( inConvexHull.size() - 1 ) ) )
+		{
+			if( inConvexHull.size() > 1 )
+			{
+				inConvexHull.remove( inConvexHull.size() - 1 );
+			}
+		}  */
 		
+		Vector2[] ret = new Vector2[ inConvexHull.size() ];
+		for( int i = 0; i < ret.length; i++ )
+		{
+			ret[i] = inConvexHull.get( i );
+		}
 		
-		return ( ( Vector2[] ) inConvexHull.toArray() );
+		return ret;
 	}
 	
 	/*-------------------------------------------------------------------------------------
