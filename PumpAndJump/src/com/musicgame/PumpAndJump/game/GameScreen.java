8f4ad866e7772a56b2d9d96706a2ab33915c5625
dtracers <dtracers@gmail.com>
2013-04-02 13:41:29 -0500

    combined the menu screen with the pregame screen

--- PumpAndJump/src/com/musicgame/PumpAndJump/game/GameScreen.java
+++ PumpAndJump/src/com/musicgame/PumpAndJump/game/GameScreen.java
@@ -1,7 +1,44 @@
 package com.musicgame.PumpAndJump.game;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.Screen;
 
-public class GameScreen implements Screen{
+public class GameScreen implements Screen
+{
+	ArrayList<GameThread> currentThreads;
+	@Override
+	public void render(float delta)
+	{
+		for(GameThread thread:currentThreads)
+		{
+			thread.render(delta);
+		}
+	}
+
+	@Override
+	public void resize(int width, int height)
+	{
+	}
+
+	@Override
+	public void show() {
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+	}
 
 }
