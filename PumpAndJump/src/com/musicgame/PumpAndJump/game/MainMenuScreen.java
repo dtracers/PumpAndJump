a9bc2a23e91418204d22c3c5570b25be3c906e97
chace <ccwork23@gmail.com>
2013-04-02 12:11:19 -0500

    basic main menu screen

---
+++ PumpAndJump/src/com/musicgame/PumpAndJump/game/MainMenuScreen.java
@@ -0,0 +1,130 @@
+package com.musicgame.PumpAndJump.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+
+
+public class MainMenuScreen implements Screen {
+    // setup the dimensions of the menu buttons
+    private static final float BUTTON_WIDTH = 300f;
+    private static final float BUTTON_HEIGHT = 60f;
+    private static final float BUTTON_SPACING = 10f;
+	Skin skin;
+	Stage stage;
+	SpriteBatch batch;
+	PumpAndJump mygame; // Note it's "MyGame" not "Game"
+ 
+
+       // constructor to keep a reference to the main Game class
+        public MainMenuScreen(PumpAndJump game){
+                this.mygame = game;
+        		batch = new SpriteBatch();
+        		stage = new Stage();
+        		Gdx.input.setInputProcessor(stage);
+
+        		// A skin can be loaded via JSON or defined programmatically, either is fine. Using a skin is optional but strongly
+        		// recommended solely for the convenience of getting a texture, region, etc as a drawable, tinted drawable, etc.
+        		skin = new Skin();
+
+        		// Generate a 1x1 white texture and store it in the skin named "white".
+        		Pixmap pixmap = new Pixmap(1, 1, Format.RGBA8888);
+        		pixmap.setColor(Color.WHITE);
+        		pixmap.fill();
+        		skin.add("white", new Texture(pixmap));
+
+        		// Store the default libgdx font under the name "default".
+        		skin.add("default", new BitmapFont());
+
+        		// Configure a TextButtonStyle and name it "default". Skin resources are stored by type, so this doesn't overwrite the font.
+        		TextButtonStyle textButtonStyle = new TextButtonStyle();
+        		textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+        		textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+        		textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+        		textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+        		textButtonStyle.font = skin.getFont("default");
+        		skin.add("default", textButtonStyle);
+
+        		// Create a table that fills the screen. Everything else will go inside this table.
+        		Table table = new Table();
+        		table.setFillParent(true);
+        		stage.addActor(table);
+
+        		// Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than "default".
+        		final TextButton button = new TextButton("Start Game!", skin);
+        		table.add(button);
+
+        		// Add a listener to the button. ChangeListener is fired when the button's checked state changes, eg when clicked,
+        		// Button#setChecked() is called, via a key press, etc. If the event.cancel() is called, the checked state will be reverted.
+        		// ClickListener could have been used, but would only fire when clicked. Also, canceling a ClickListener event won't
+        		// revert the checked state.
+        		button.addListener(
+        				new ChangeListener() { public void changed (ChangeEvent event, Actor actor) { button.setText("Good job!"); mygame.setScreen(mygame.gameScreen); } });
+
+        		// Add an image actor. Have to set the size, else it would be the size of the drawable (which is the 1x1 texture).
+        		table.add(new Image(skin.newDrawable("white", Color.RED))).size(64);
+        }
+        
+        @Override
+        public void render(float delta) {
+    		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+    		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+    		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+    		stage.draw();
+    		Table.drawDebug(stage);
+        	//check for input
+             //if (Gdx.input.justTouched()) // use your own criterion here
+               //  game.setScreen(game.gameScreen);
+        }
+ 
+
+       @Override
+       public void resize (int width, int height) {
+           stage.setViewport(width, height, false);
+   }
+ 
+
+       @Override
+        public void show() {
+             // called when this screen is set as the screen with game.setScreen();
+        }
+ 
+
+       @Override
+        public void hide() {
+             // called when current screen changes from this to a different screen
+        }
+ 
+
+       @Override
+        public void pause() {
+        }
+ 
+
+       @Override
+        public void resume() {
+        }
+ 
+
+       @Override
+        public void dispose() {
+                // never called automatically
+   		stage.dispose();
+   		skin.dispose();
+        }
+ }
