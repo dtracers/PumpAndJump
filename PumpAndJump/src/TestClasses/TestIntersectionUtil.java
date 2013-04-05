fb81325e522bf4a7f64d14daa3041bf0c9b4a1fa
Matthew Bulluck <mr.datonana@gmail.com>
2013-04-05 10:08:28 -0500

    Added CreateConvexHull() and GrahamsScan() Tests

--- PumpAndJump/src/TestClasses/TestIntersectionUtil.java
+++ PumpAndJump/src/TestClasses/TestIntersectionUtil.java
@@ -6,6 +6,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
 
 import com.musicgame.PumpAndJump.Util.IntersectionUtil;
@@ -15,9 +16,9 @@ public class TestIntersectionUtil {
 	public ExpectedException exception = ExpectedException.none();
 	@Test
 	public void testVector2ToFloat() {
-		Vector2[] TestCase={new Vector2(1,2),new Vector2(3,4)};
-		float [] ExpResult={1,2,3,4};
-		String ErrorMessage="IntersectionUtil::testVector2ToFloat-";
+		Vector2[] TestCase={new Vector2(1.0f,2.0f),new Vector2(3.0f,4.0f)};
+		float [] ExpResult={1.0f,2.0f,3.0f,4.0f};
+		String ErrorMessage="IntersectionUtil::Vector2ToFloat-";
 		
 		float [] Result=IntersectionUtil.Vector2ToFloat(TestCase);
 		
@@ -34,9 +35,9 @@ public class TestIntersectionUtil {
 	@Test
 	public void testFloatToVector2() {
 		float [] OddTest={0};
-		float [] TestCase={1,2,3,4};
-		Vector2[] ExpResult={new Vector2(1,2),new Vector2(3,4)};
-		String ErrorMessage="IntersectionUtil::testFloatToVector2:";
+		float [] TestCase={1.0f,2.0f,3.0f,4.0f};
+		Vector2[] ExpResult={new Vector2(1.0f,2.0f),new Vector2(3.0f,4.0f)};
+		String ErrorMessage="IntersectionUtil::FloatToVector2:";
 		
 		Vector2[] Result=IntersectionUtil.FloatToVector2(TestCase);
 		assertEquals(ErrorMessage+"Invalid return length",ExpResult.length,Result.length);
@@ -54,12 +55,25 @@ public class TestIntersectionUtil {
 
 	@Test
 	public void testCreateConvexHull() {
-		fail("Not yet implemented");
+		//need more info to create Valid test case
+		float [] TestArray={1.0f,2.0f,3.0f,4.0f};
+		Polygon TestCase=new Polygon(TestArray);
+		String ErrorMessage="IntersectionUtil::CreateConvexHull-";
+		float [] Result=IntersectionUtil.createConvexHull(TestCase);
+		
+		assertTrue(ErrorMessage+" Invalid return array length",Result.length<=TestArray.length);
+		exception.expect(RuntimeException.class);
+		IntersectionUtil.createConvexHull(null);
 	}
 
 	@Test
 	public void testGrahamScan() {
-		fail("Not yet implemented");
+		Vector2 [] tooFew={new Vector2(0.0f,0.0f)};
+		String ErrorMessage="IntersectionUtil::GrahamScan-";
+		
+		assertNull(ErrorMessage+" Non null returned for too few points.",IntersectionUtil.grahamScan(tooFew));
+		exception.expect(RuntimeException.class);
+		IntersectionUtil.grahamScan(null);
 	}
 
 }
