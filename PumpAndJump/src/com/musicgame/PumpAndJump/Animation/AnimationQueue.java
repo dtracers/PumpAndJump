c59a2476d0bcd59664eff3856d9fab1e8c5d75a1
Christopher A. Trask <chris.trask@gmail.com>
2013-04-13 13:13:53 -0500

    Added new AnimationQueue Class
    Its starting to really come together guys nice job!

---
+++ PumpAndJump/src/com/musicgame/PumpAndJump/Animation/AnimationQueue.java
@@ -0,0 +1,15 @@
+package com.musicgame.PumpAndJump.Animation;
+
+public class AnimationQueue {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
