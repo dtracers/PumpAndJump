43fa97f668b22731dccb8826c00e8c0e126dea5d
dtracers <dtracers@gmail.com>
2013-04-18 12:48:34 -0500

    need to make the fille Chooser as a state within the game

---
+++ PumpAndJump/src/com/musicgame/PumpAndJump/game/gameStates/FileChooser.java
@@ -0,0 +1,41 @@
+package com.musicgame.PumpAndJump.game.gameStates;
+
+import com.musicgame.PumpAndJump.game.GameThread;
+import com.musicgame.PumpAndJump.game.ThreadName;
+
+public class FileChooser extends GameThread
+{
+
+	//need to make a file chooser
+	@Override
+	public void render(float delta)
+	{
+		//will draw all of the files that are available
+	}
+
+	@Override
+	public ThreadName getThreadName() {
+		return null;
+	}
+
+	@Override
+	public void unpause() {
+	}
+
+	@Override
+	public void repause() {
+	}
+
+	@Override
+	public void switchFrom(GameThread currentThread) {
+	}
+
+	@Override
+	public void addFrom(GameThread currentThread) {
+	}
+
+	@Override
+	public void removeFrom(GameThread currentThread) {
+	}
+
+}
