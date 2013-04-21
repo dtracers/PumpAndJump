e5b51dbf498c3bec114a13c9a5397967278f71db
dtracers <dtracers@gmail.com>
2013-04-20 22:49:27 -0500

    changed this a lot

--- SpectroEdit/src/net/bluecow/spectro/detection/tempo/DistancePaint.java
+++ SpectroEdit/src/net/bluecow/spectro/detection/tempo/DistancePaint.java
@@ -9,24 +9,27 @@ import javax.swing.JPanel;
 
 public class DistancePaint extends JPanel
 {
-	public static void main(String args[])
+	public static void main(String args[]) throws InterruptedException
 	{
+		String file = "The Hand That Feeds - Nine Inch Nails.mp3";
 		RegressionDetection detector = new RegressionDetection(null);
-		detector.createBeatsFromAFile("The Hand That Feeds - Nine Inch Nails.mp3");
+		detector.createBeatsFromAFile(file);
 		DistancePaint p = new DistancePaint();
+		p.setBackground(Color.white);
 		detector.painter =p;
-		JFrame f = createJFrame(p);
-		for(int k=0;k<1;k++)
+		JFrame f = createJFrame(p,file);
+		for(int k=0;k<detector.detectedBeats.size();k++)
 		{
 			detector.detectTempo(k);
+			Thread.sleep(100);
 			f.repaint();
 		}
 		f.repaint();
 	}
 
-	public static JFrame createJFrame(DistancePaint p)
+	public static JFrame createJFrame(DistancePaint p,String name)
 	{
-		JFrame frame = new JFrame();
+		JFrame frame = new JFrame(name);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(false);
 		frame.setSize(900,900);
@@ -41,8 +44,12 @@ public class DistancePaint extends JPanel
 	int offY = 10;
 	double[] line;
 	ArrayList<Distance> distances;
-	public void PaintComponent(Graphics g)
+
+	@Override
+	public void paintComponent(Graphics g)
 	{
+		g.setColor(Color.white);
+		g.fillRect(0, 0, width, height);
 		if(distances!=null)
 		{
 			int length = distances.size();
@@ -62,8 +69,13 @@ public class DistancePaint extends JPanel
 				double a = line[0];
 				double b = line[1];
 				double R2 = line[2];
-				g.setColor(Color.blue);
-				g.drawLine((int)offX,(int) (height-(a*0+b+offY)), (int)(width+offX),(int) (height-(a*offY+b+offY)));
+				if(R2>.998)
+				{
+					g.setColor(Color.blue);
+					g.drawLine((int)offX,(int) (height-(a*0+b+offY)), (int)(width+offX),(int) (height-(a*distances.size()+b+offY)));
+				}
+					g.drawString("Y = "+a+"*x"+b, 0, 20);
+					g.drawString("R = "+R2, 0, 30);
 			}
 		}else
 		{
