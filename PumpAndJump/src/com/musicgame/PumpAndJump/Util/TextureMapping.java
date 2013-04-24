bda8417fdd23971dbb2045902fec2bf9b88eaf97
dtracers <dtracers@gmail.com>
2013-04-24 14:47:26 -0500

    everything is lower case
    MAKE SURE ALL FILES ARE LOWER CASE AND LOWER CASE ONLY!

--- PumpAndJump/src/com/musicgame/PumpAndJump/Util/TextureMapping.java
+++ PumpAndJump/src/com/musicgame/PumpAndJump/Util/TextureMapping.java
@@ -8,44 +8,44 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
-public class TextureMapping 
+public class TextureMapping
 {
 	public static TextureMapping textures;
 	public Map< String, Texture > nameToTexture;
 	public TextureAtlas textureAtlas;
-	
+
 	public TextureMapping()
 	{
 		nameToTexture = new HashMap< String, Texture >();
-		textureAtlas = new TextureAtlas( "playerSprite.txt" );
+		textureAtlas = new TextureAtlas( "playersprite.txt" );
 	}
-	
+
 	public static void constructStaticMapping()
 	{
 		textures = new TextureMapping();
-		
+
 	}
-	
+
 	public int addTexture( String textureStr )
 	{
 		Texture tex = new Texture( Gdx.files.internal( textureStr ) );
-		
+
 		if( nameToTexture.get( textureStr ) == null )
 		{
 			nameToTexture.put( textureStr, tex  );
 			return 1;
 		}
-		
+
 		return 0;
 	}
-	
+
 	public static int staticAddTexture( String textureStr )
 	{
 		if( textures != null )
 			return textures.addTexture( textureStr );
 		return -1;
 	}
-	
+
 	public static Texture staticGet( String textureStr )
 	{
 		if( textures != null )
@@ -55,14 +55,14 @@ public class TextureMapping
 			{
 				return t;
 			}
-			
+
 			staticAddTexture( textureStr );
-			
+
 			return textures.get( textureStr );
 		}
 		return null;
 	}
-	
+
 	public static Sprite staticGetSprite( String spriteName )
 	{
 
@@ -76,14 +76,14 @@ public class TextureMapping
 		}
 		return null;
 	}
-	
+
 	public Texture get( String textureStr )
 	{
 		if( nameToTexture.get( textureStr ) != null )
 			return nameToTexture.get( textureStr );
 		return null;
 	}
-	
+
 	public Sprite getSprite( String spriteName )
 	{
 		return textureAtlas.createSprite( spriteName );
