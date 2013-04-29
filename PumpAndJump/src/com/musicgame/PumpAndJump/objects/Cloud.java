0cb07afb97f874e02271b08ed78eeed6f0fca7fa
dtracers <dtracers@gmail.com>
2013-04-29 03:21:50 -0500

    cload added

---
+++ PumpAndJump/src/com/musicgame/PumpAndJump/objects/Cloud.java
@@ -0,0 +1,19 @@
+package com.musicgame.PumpAndJump.objects;
+
+import com.badlogic.gdx.math.Matrix4;
+
+public class Cloud extends Obstacle
+{
+
+	public Cloud(float Start, float End, float Y, float Height)
+	{
+		super(Start, End, Y, Height,"cloud.png");
+	}
+
+	//updates location
+	public void update( Matrix4 m, float delta )
+	{
+		super.update( m, delta);
+	}
+
+}
